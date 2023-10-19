cd4bface1252155808a7cff54fc5e5296d01f678
hexcharlie <luishenriquemf23@hotmail.com>
2023-10-19 15:13:21 -0300

    modificações produtoCard e form Contato

--- src/components/contato/Contato.tsx
+++ src/components/contato/Contato.tsx
@@ -1,60 +1,77 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import { toastAlerta } from '../../utils/toastAlerta';
+
 
 function Contato() {
   const [state, handleSubmit] = useForm("xjvqjnaz");
 
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await handleSubmit(event);
+
+    if (response === null) {
+      toastAlerta("Email enviado", "sucesso");
+    } else {
+      toastAlerta("Algo deu errado!", "erro");
+    }
   }
 
+  if (state.succeeded) {
+    return (
+      <div className="flex justify-center items-center h-screen font-bold"> 
+        <p>Retornaremos sua mensagem em breve!</p>
+      </div>
+    );
+  }
   return (
     <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="w-full max-w-md">
+      <form onSubmit={handleFormSubmit} className="w-full max-w-md">
         <label htmlFor="name" className="block text-gray-700 text-sm font-bold mt-4 mb-2">
           Nome
         </label>
         <input
           id="name"
-          type="text" 
+          type="text"
           name="name"
-          className="w-full p-2 border rounded"
+          className="w-full p-2 border border-green-500 rounded"
         />
-        <ValidationError 
-          prefix="Name" 
+        <ValidationError
+          prefix="Name"
           field="name"
           errors={state.errors}
         />
-        
+  
         <label htmlFor="email" className="block text-gray-700 text-sm font-bold mt-4 mb-2">
           Email
         </label>
         <input
           id="email"
-          type="email" 
+          type="email"
           name="email"
-          className="w-full p-2 border rounded"
+          className="w-full p-2 border border-green-500 rounded"
         />
-        <ValidationError 
-          prefix="Email" 
+        <ValidationError
+          prefix="Email"
           field="email"
           errors={state.errors}
         />
-
+  
         <label htmlFor="message" className="block text-gray-700 text-sm font-bold mt-4 mb-2">
           Mensagem
         </label>
         <textarea
           id="message"
           name="message"
-          className="w-full p-2 border rounded"
+          className="w-full p-2 border border-green-500 rounded"
         />
-        <ValidationError 
-          prefix="Message" 
+        <ValidationError
+          prefix="Message"
           field="message"
           errors={state.errors}
         />
-
+  
         <button type="submit" disabled={state.submitting} className="w-full p-2 bg-green-500 text-white rounded mt-4">
           Enviar
         </button>
--- src/components/produtos/cardProdutos/CardProdutos.tsx
+++ src/components/produtos/cardProdutos/CardProdutos.tsx
@@ -1,41 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Produto from '../../../models/Produto'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Produto from '../../../models/Produto';
+
+import './CardProduto.css'
 
 interface CardProdutoProps {
-  produto: Produto
+  produto: Produto;
 }
 
-function CardProduto({produto}: CardProdutoProps) {
+function CardProduto({ produto }: CardProdutoProps) {
   return (
-    <div className='border-slate-900 border flex flex-col rounded overflow-hidden justify-between'>
-      <div>
+    <div>
+      <div className='border-slate-900 border flex flex-col rounded overflow-hidden'>
         <div className="flex w-full bg-green-600 py-2 px-4 items-center gap-4">
           <img src={produto.usuario?.foto} className='h-12 rounded-full' alt="" />
           <h3 className='text-lg font-bold text-center uppercase '>{produto.usuario?.nome}</h3>
         </div>
-        <div className='p-4 '>
-          <h4 className='text-lg font-semibold uppercase'>{produto.nome}</h4>
-          <p>{produto.descricao}</p>
-          <p> {produto.preco.toLocaleString("pt-BR",{style:"currency", currency:"BRL"})}</p>
-          <img src={produto.foto} alt="" className='rounded w-36 mx-auto'/>
-          <p>Categoria: {produto.categoria?.nome}</p>
-          {/* <p>Data: {new Intl.DateTimeFormat(undefined, {
-                    dateStyle: 'full',
-                    timeStyle: 'medium',
-                  }).format(new Date(produto.data))}</p> */}
+
+        <div className="flex">
+          <div className='p-4'>
+            <img src={produto.foto} alt="" className='rounded fixed-image' />
+          </div>
+          <div className='p-4 flex flex-col'>
+            <h4 className='text-lg font-semibold uppercase'>{produto.nome}</h4>
+            <p>{produto.descricao}</p>
+            <p>{produto.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
+            <p>Categoria: {produto.categoria?.nome}</p>
+          </div>
+        </div>
+        <div className="flex mt-auto">
+          <Link to={`/editarProduto/${produto.id}`} className='w-full text-white bg-green-400 hover:bg-green-800 flex items-center justify-center py-2'>
+            <button>Editar</button>
+          </Link>
+          <Link to={`/deletarProduto/${produto.id}`} className='text-white bg-red-400 hover-bg-red-700 w-full flex items-center justify-center'>
+            <button>Deletar</button>
+          </Link>
         </div>
       </div>
-      <div className="flex">
-      <Link to={`/editarProduto/${produto.id}`} className='w-full text-white bg-green-400 hover:bg-green-800 flex items-center justify-center py-2'>
-          <button>Editar</button>
-        </Link>
-        <Link to={`/deletarProduto/${produto.id}`} className='text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
-          <button>Deletar</button>
-        </Link>
-      </div>
     </div>
-  )
+  );
 }
 
-export default CardProduto
+export default CardProduto;
